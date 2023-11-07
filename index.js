4b60ae3af905568375bd7480c5c878a5edd1f857
karl ducta <karlducta022@gmail.com>
2023-11-08 01:30:47 +0800

    Mutation --- Update Data

--- index.js
+++ index.js
@@ -61,6 +61,17 @@ const resolvers = {
 
       return game;
     },
+    updateGame(_, args) {
+      db.games = db.games.map((game) => {
+        if (game.id === args.id) {
+          return { ...game, ...args.edits };
+        }
+
+        return game;
+      });
+
+      return db.games.find((game) => game.id === args.id);
+    },
   },
 };
 
