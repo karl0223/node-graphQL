e6e4b6b7bba07d3dcf99faec0b85dd1a5a05efa8
karl ducta <karlducta022@gmail.com>
2023-11-07 23:40:18 +0800

    Resolvers Functions -- how to fetch data

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+// db
+import db from "./_db.js";
+
 // types
 import { typeDefs } from "./schema.js";
+
+const resolvers = {
+  Query: {
+    authors() {
+      return db.authors;
+    },
+    games() {
+      return db.games;
+    },
+    reviews() {
+      return db.reviews;
+    },
+  },
+};
 
 // server setup
 
 const server = new ApolloServer({
   typeDefs, // schema, type of data
-  //resolvers,  // how to fetch data
+  resolvers, // how to fetch data
 });
 
 const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
