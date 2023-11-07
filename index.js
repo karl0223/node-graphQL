7e7d3d139c78949f6661ecd16f85237339d59042
karl ducta <karlducta022@gmail.com>
2023-11-07 23:23:12 +0800

    Schema and types

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+// types
+import { typeDefs } from "./schema.js";
+
 // server setup
 
 const server = new ApolloServer({
-  //typeDefs,
-  //resolvers,
+  typeDefs, // schema, type of data
+  //resolvers,  // how to fetch data
 });
 
 const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
